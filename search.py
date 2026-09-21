10cae8ac55d685e3be00e92eaae9e5f00e6063c6
Hayley Smith <hayleyamandasmith@googlemail.com>
2026-09-21 18:21:46 +0900

    Refactor search into functions

--- search.py
+++ search.py
@@ -4,32 +4,41 @@ conn = sqlite3.connect("doctor_who.db")
 
 cursor = conn.cursor()
 
-while True:
-    try:
-        season = int(input("Which season? "))
-        break
-    except ValueError:
-        print("Please enter a number")
+def get_season():
+    while True:
+        try:
+            season = int(input("Which season? "))
+            return season
+        except ValueError:
+            print("Please enter a number")
 
-cursor.execute("""
-    SELECT * 
-    FROM episodes 
-    WHERE season = ? 
-    ORDER BY rating DESC
-    LIMIT 10
-""",(season,))
-rows = cursor.fetchall()
+def search_db(cursor, season):
+    cursor.execute("""
+        SELECT * 
+        FROM episodes 
+        WHERE season = ? 
+        ORDER BY rating DESC
+        LIMIT 10
+    """,(season,))
+    rows = cursor.fetchall()
+    return rows
 
-if not rows:
-    print("No episodes found for this season.")
-else:
-    print(f"\nTop episodes from Season {season}\n")
+season = get_season()
+rows = search_db(cursor, season)
 
-    for row in rows:
-        season_number, episode_number, title, rating = row
-        print(f"Season {season_number}, Episode {episode_number}")
-        print(title)
-        print(f"Rating: {rating}")
-        print("-" * 30)
+def display_results(rows, season):
+    if not rows:
+        print("No episodes found for this season.")
+    else:
+        print(f"\nTop episodes from Season {season}\n")
+
+        for row in rows:
+            season_number, episode_number, title, rating = row
+            print(f"Season {season_number}, Episode {episode_number}")
+            print(title)
+            print(f"Rating: {rating}")
+            print("-" * 30)
+            
+display_results(rows, season)
 
 conn.close()
